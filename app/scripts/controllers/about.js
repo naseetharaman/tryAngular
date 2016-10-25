53f867eb899fbd1d7f8a1b1594f0131b655a1053
naseetharaman <naseetharaman@paypal.com>
2016-10-25 23:25:14 +0530

    Sample App for Angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cafeAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cafeAppApp
+ */
+angular.module('cafeAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cafeAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cafeAppApp
+ */
+angular.module('cafeAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
